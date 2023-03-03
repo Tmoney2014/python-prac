361ca4682a0997b51a89e8161d3358784639bcdd
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2023-03-03 23:23:50 +0900

    2강 , 3강

---
+++ prac/prac.py
@@ -0,0 +1,23 @@
+# 실습 2강
+print("hello world")
+print(200 - 1)
+print(1000000 - 2222222 / 3)
+print(3.5 - 1 / 3 * 33)
+if 4 > 3:
+    print("true")
+print("자바랑 다르네")
+
+# print(format())
+
+a , b = 1 , 3
+print(a)
+
+a , b = b , a
+print(a , b)
+
+print(12 // 5)
+print(5 ** 3)
+
+a = 1
+a += 2
+print(a)
